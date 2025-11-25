952926909f5c4538f3dded7b82dd9289e4c1e141
Manager <manager.companyy92@gmail.com>
2025-11-26 04:04:45 +0500

    Update catalogCategories.js

--- src/utils/catalogCategories.js
+++ src/utils/catalogCategories.js
@@ -4,45 +4,45 @@ const catalogCategories = [
   {
     id: "kitchens",
     name: "Кухни",
-    cover: "/public/catalog/cat-kitchens.jpg",
+    image: "/public/catalog/cat-kitchens.jpg",
     tagline: "Современные кухни под заказ",
-    statsLabel: "33 проекта"
+    statsLabel: "33 проекта",
   },
   {
     id: "wardrobes",
     name: "Гардеробные",
-    cover: "/public/catalog/cat-wardrobes.jpg",
+    image: "/public/catalog/cat-wardrobes.jpg",
     tagline: "Функциональные гардеробные любой сложности",
-    statsLabel: "44 проекта"
+    statsLabel: "44 проекта",
   },
   {
     id: "bedrooms",
     name: "Спальни",
-    cover: "/public/catalog/cat-bedrooms.jpg",
+    image: "/public/catalog/cat-bedrooms.jpg",
     tagline: "Комфортные и атмосферные спальные зоны",
-    statsLabel: "38 проектов"
+    statsLabel: "38 проектов",
   },
   {
     id: "livingrooms",
     name: "Гостиные",
-    cover: "/public/catalog/cat-livingrooms.jpg",
+    image: "/public/catalog/cat-livingrooms.jpg",
     tagline: "Элегантные ТВ-зоны и стеновые композиции",
-    statsLabel: "33 проекта"
+    statsLabel: "33 проекта",
   },
   {
     id: "hallways",
     name: "Прихожие",
-    cover: "/public/catalog/cat-hallways.jpg",
+    image: "/public/catalog/cat-hallways.jpg",
     tagline: "Уютные и практичные прихожие под ключ",
-    statsLabel: "37 проектов"
+    statsLabel: "37 проектов",
   },
   {
     id: "kids",
     name: "Детские",
-    cover: "/public/catalog/cat-kids.jpg",
+    image: "/public/catalog/cat-kids.jpg",
     tagline: "Эргономичные детские комнаты для любого возраста",
-    statsLabel: "46 проектов"
-  }
+    statsLabel: "46 проектов",
+  },
 ];
 
 export default catalogCategories;
