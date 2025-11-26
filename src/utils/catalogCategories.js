9ccf3e5deda5e2da0610da67f1297414d04de011
Manager <manager.companyy92@gmail.com>
2025-11-26 06:00:25 +0500

    Update catalogCategories.js

--- src/utils/catalogCategories.js
+++ src/utils/catalogCategories.js
@@ -2,47 +2,33 @@
 
 export const catalogCategories = [
   {
-    id: "kitchens",
-    name: "Кухни",
-    image: "/public/catalog/kitchens-cover.jpg",
-    tagline: "Современные кухни под заказ",
-    statsLabel: "33 проекта",
+    id: "wardrobes",
+    name: "Гардеробные системы",
+    image: "/catalog/wardrobes-cover.jpg",
   },
   {
-    id: "wardrobes",
-    name: "Гардеробные",
-    image: "/public/catalog/wardrobes-cover.jpg",
-    tagline: "Функциональные гардеробные любой сложности",
-    statsLabel: "44 проекта",
+    id: "kitchens",
+    name: "Кухни на заказ",
+    image: "/catalog/kitchens-cover.jpg",
   },
   {
     id: "bedrooms",
-    name: "Спальни",
-    image: "/public/catalog/bedrooms-cover.jpg",
-    tagline: "Комфортные и атмосферные спальные зоны",
-    statsLabel: "38 проектов",
-  },
-  {
-    id: "livingrooms",
-    name: "Гостиные",
-    image: "/public/catalog/livingrooms-cover.jpg",
-    tagline: "Элегантные ТВ-зоны и стеновые композиции",
-    statsLabel: "33 проекта",
-  },
-  {
-    id: "hallways",
-    name: "Прихожие",
-    image: "/public/catalog/hallways-cover.jpg",
-    tagline: "Уютные и практичные прихожие под ключ",
-    statsLabel: "37 проектов",
+    name: "Спальные зоны",
+    image: "/catalog/bedrooms-cover.jpg",
   },
   {
     id: "kids",
-    name: "Детские",
-    image: "/public/catalog/kids-cover.jpg",
-    tagline: "Эргономичные детские комнаты для любого возраста",
-    statsLabel: "46 проектов",
+    name: "Детские комнаты",
+    image: "/catalog/kids-cover.jpg",
+  },
+  {
+    id: "hallways",
+    name: "Прихожие и холлы",
+    image: "/catalog/hallways-cover.jpg",
+  },
+  {
+    id: "livingrooms",
+    name: "Гостиные интерьеры",
+    image: "/catalog/livingrooms-cover.jpg",
   },
 ];
-
-export default catalogCategories;
