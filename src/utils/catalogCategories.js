93001b5501950746c5db97ba7871b68294c6c1e6
Manager <manager.companyy92@gmail.com>
2025-11-26 06:03:50 +0500

    Update catalogCategories.js

--- src/utils/catalogCategories.js
+++ src/utils/catalogCategories.js
@@ -3,32 +3,32 @@
 export const catalogCategories = [
   {
     id: "wardrobes",
-    name: "Гардеробные системы",
-    image: "/catalog/wardrobes-cover.jpg",
+    name: "Гардеробные",
+    image: "/public/catalog/wardrobes-cover.jpg",
   },
   {
     id: "kitchens",
-    name: "Кухни на заказ",
-    image: "/catalog/kitchens-cover.jpg",
+    name: "Кухонные",
+    image: "/public/catalog/kitchens-cover.jpg",
   },
   {
     id: "bedrooms",
-    name: "Спальные зоны",
-    image: "/catalog/bedrooms-cover.jpg",
+    name: "Спальные",
+    image: "/public/catalog/bedrooms-cover.jpg",
   },
   {
     id: "kids",
-    name: "Детские комнаты",
-    image: "/catalog/kids-cover.jpg",
+    name: "Детские",
+    image: "/public/catalog/kids-cover.jpg",
   },
   {
     id: "hallways",
-    name: "Прихожие и холлы",
-    image: "/catalog/hallways-cover.jpg",
+    name: "Прихожие",
+    image: "/public/catalog/hallways-cover.jpg",
   },
   {
     id: "livingrooms",
-    name: "Гостиные интерьеры",
-    image: "/catalog/livingrooms-cover.jpg",
+    name: "Гостиные",
+    image: "/public/catalog/livingrooms-cover.jpg",
   },
 ];
