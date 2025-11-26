4d8e79a4929824f023d56bc709dccb49a8421ca7
Manager <manager.companyy92@gmail.com>
2025-11-26 07:18:37 +0500

    Update index.js

--- src/pages/catalog/index.js
+++ src/pages/catalog/index.js
@@ -1,19 +1,6 @@
 // src/pages/catalog/index.js
+import CatalogGrid from "@/components/CatalogGrid";
 
 export default function CatalogPage() {
-  return (
-    <main
-      style={{
-        minHeight: "100vh",
-        background: "#000",
-        color: "#fff",
-        fontSize: "24px",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "center",
-      }}
-    >
-      ТЕСТ /catalog
-    </main>
-  );
+  return <CatalogGrid />;
 }
