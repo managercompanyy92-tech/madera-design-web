4d324db584290b806c0470058bf59f3e98503e05
Manager <manager.companyy92@gmail.com>
2025-12-07 02:03:05 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ const BASE_RATES = {
   premium: 5000,
 };
 // --- Telegram settings ---
-const TG_BOT_TOKEN = '7246110807:AAGksT-IRrKFx1QeaHl-KxcT5nfdOPrH-0U';
+const TG_BOT_TOKEN = '7246110807:AAEDQq0PpjW6IAno7iJUHYFh40UWluvCQBE';
 const TG_CHAT_ID = 7889419635; // ваш user id из @userinfobot
 
 const TG_API_URL = `https://api.telegram.org/bot${TG_BOT_TOKEN}/sendMessage`;
