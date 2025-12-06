84d49ae32faff5aad622dea2a5a12e7b860ca438
Manager <manager.companyy92@gmail.com>
2025-12-06 23:27:35 +0500

    Update main.js

--- src/main.js
+++ src/main.js
@@ -197,7 +197,7 @@ function renderHome() {
           transform: scale(1);
         }
         .hero-photo {
-        margin-top: -64px !important; /* подтягиваем блок с фото вверх */
+  transform: translateY(-26px);
       }
     </style>
 
